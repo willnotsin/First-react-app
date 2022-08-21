0add45b309e09c4c5ee51c0b6f1c9dbdde6ac3d9
willnotsin <wedafe45@gmail.com>
2022-08-21 22:12:06 +0100

    Create container component and edit the css files

---
+++ src/Container.js
@@ -0,0 +1,12 @@
+export default function Container(prop){
+    const {children} = prop
+    
+    return (
+        <>
+            <div className="container">
+                {children}
+            </div>
+        </>
+    )
+
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
 import App from './App';
 
@@ -7,4 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
 );
+
+// OR 
+//ReactDOM.render(<App />, document.getElementById('root'));
 
