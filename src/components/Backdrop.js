6279815cbace291e2f93d14b09c797503d554fd5
willnotsin <wedafe45@gmail.com>
2022-08-22 03:34:32 +0100

    Added onClick to the component

--- src/components/Backdrop.js
+++ src/components/Backdrop.js
@@ -1,3 +1,4 @@
-export default function Backdrop(){
-    return <div className="backdrop" />
+export default function Backdrop(prop){
+    const {onClick} = prop
+    return <div className="backdrop" onClick={onClick}/>
 }
