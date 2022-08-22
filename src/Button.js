ef2b147353c49e0d4646064fc8f8c7e5c47fc365
willnotsin <wedafe45@gmail.com>
2022-08-22 03:29:13 +0100

    created  component folder

--- src/Button.js
+++
@@ -1,9 +0,0 @@
-export default function Button(prop) {
-  const { type = "submit", children, disabled } = prop;
-
-  return (
-    <button type={type} disabled={disabled} className="btn">
-      {children}
-    </button>
-  );
-}
