8f9c438939c6bee913e7428b35e8263cb2ea3432
willnotsin <wedafe45@gmail.com>
2022-08-22 03:30:47 +0100

    Nothing much, just the work

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
-// OR 
+// OR
 //ReactDOM.render(<App />, document.getElementById('root'));
-
